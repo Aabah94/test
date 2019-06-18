5846b366b12bc64e96912b68d5b7a4d502cbbb62
Aabah94 <47247251+Aabah94@users.noreply.github.com>
2019-06-19 01:13:29 +0530

    Update CustomerServiceTest.java

--- src/test/java/com/codecentric/sample/store/service/CustomerServiceTest.java
+++ src/test/java/com/codecentric/sample/store/service/CustomerServiceTest.java
@@ -20,7 +20,7 @@ import org.mockito.stubbing.Answer;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
-import com.agiletestware.bumblebee.annotations.BumblebeeJUnitListener;
+import com.agiletestware.bumblebee.annotations.*;
 import java.util.ArrayList;
 import java.util.List;
 
