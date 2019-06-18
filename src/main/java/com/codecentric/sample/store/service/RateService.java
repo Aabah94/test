c5b9fd0fbc2544345c5154f5cf6fb10d75abd8bc
Aabah94 <47247251+Aabah94@users.noreply.github.com>
2019-06-19 01:21:57 +0530

    Update RateService.java

--- src/main/java/com/codecentric/sample/store/service/RateService.java
+++ src/main/java/com/codecentric/sample/store/service/RateService.java
@@ -5,6 +5,13 @@ import com.codecentric.sample.store.repository.ItemRepository;
 import com.codecentric.sample.store.service.tools.StaticService;
 import org.springframework.beans.factory.annotation.Autowired;
 
+
+import com.agiletestware.bumblebee.annotations.BooleanValue;
+import com.agiletestware.bumblebee.annotations.Bumblebee;
+import com.agiletestware.bumblebee.annotations.BumblebeeJUnitListener;
+
+
+    @Bumblebee(testlab = "Root\\TestBumbleBee", testset = "TestBumbleBee", testplan = "Subject\\TestBumbleBee")
 public class RateService {
 
     @Autowired
